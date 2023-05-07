7873a6fdc0d30060f5f513b284306e6937cc53ad
philipheferreira <philipheferreira@gmail.com>
2023-05-07 15:33:16 -0300

    update logica simples relogio

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.querySelector(".teste").innerHTML = 'testando';
+var displayHoras = document.querySelector(".horas");
+var displayMinutos = document.querySelector(".minutos");
+var displaySegundos = document.querySelector(".segundos");
+
+setInterval(function(){
+
+	var tempo = new Date();
+	var hr = tempo.getHours();
+	var mn = tempo.getMinutes();
+	var sd = tempo.getSeconds();
+
+	displayHoras.textContent = hr;
+	displayMinutos.textContent = mn;
+	displaySegundos.textContent = sd;
+});
